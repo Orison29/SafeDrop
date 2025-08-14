84dc3a1711b354af3f041c83d32b7f60d0362f4d
Orison Bachute <orisonbachute1222@gmail.com>
2025-08-14 20:35:46 +0530

    add uploads feat

--- server/routes/file-routes.js
+++ server/routes/file-routes.js
@@ -1,9 +1,54 @@
+// fileRoutes.js
 const express = require('express');
+const multer = require('multer');
+const path = require('path');
+const fs = require('fs');
+const file = require('../models/file-model')
+const authMiddleware = require('../middleware/auth-middleware');
+
 const router = express.Router();
 
-// Example route
-router.get('/ping', (req, res) => {
-  res.send('pong');
+// Multer storage config
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'uploads/'); // store in uploads folder
+    },
+    filename: (req, file, cb) => {
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+        cb(null, uniqueSuffix + path.extname(file.originalname));
+    }
+});
+
+const upload = multer({ storage });
+
+// @desc Upload a file
+// @route POST /api/files/upload
+// @access Private
+// Upload file
+
+router.post('/upload', authMiddleware, upload.single('file'), async (req, res) => {
+    try {
+        if (!req.file) {
+            return res.status(400).json({ message: 'No file uploaded' });
+        }
+
+        const newFile = new file({
+            filename: req.file.filename,
+            originalname: req.file.originalname,
+            mimetype: req.file.mimetype,
+            size: req.file.size,
+            uploadedBy: req.user.userId
+        });
+
+        await newFile.save();
+
+        res.status(201).json({
+            message: 'File uploaded successfully',
+            file: newFile
+        });
+    } catch (error) {
+        res.status(500).json({ message: 'Server error', error: error.message });
+    }
 });
 
 module.exports = router;
